2ae4f370b4395af0bf0d8f88592d498d3950795b
psure10 <84059375+PowerDragon2451@users.noreply.github.com>
2026-05-23 20:19:48 -0500

    Added some significant UI changes from going from a dark pink theme to a more darker theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { GetInputForm } from './components/GetInputForm';
 import { ShowPlayerList } from './components/ShowPlayerList';
-
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Tournament Groups</h1>
-      <div className="InputForm">
-        <GetInputForm labelProps={{ labelName: "Player Name: " }} inputProps={{ inputType: "text", inputId: "playerInput", onChange: () => { } }} buttonprops={{ buttonLabel: "Submit", onclick: () => { } }} />
+      <div className="Layout">
+        <div className="leftSideBar">
+          
+        </div>
+        <div className="mainBody">
+          <div className="InputForm">
+            <GetInputForm labelProps={{ labelName: "Player Name: " }} inputProps={{ inputType: "text", inputId: "playerInput", onChange: () => { } }} buttonprops={{ buttonLabel: "Submit", onclick: () => { } }} />
+          </div>
+          <ShowPlayerList></ShowPlayerList>
+        </div>
+        <div className="rightSideBar"></div>
       </div>
-      <ShowPlayerList></ShowPlayerList>
-      
     </div>
   );
 }
